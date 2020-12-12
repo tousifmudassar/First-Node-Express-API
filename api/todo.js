3f494a3cc3f34e962f562fd8b915361bd11bc21b
Mudassar Tousif <tousifmudassar@gmail.com>
2020-12-12 18:43:37 +0530

    Implemented  PUT request for updating ToDo Item.

--- api/todo.js
+++ api/todo.js
@@ -3,7 +3,13 @@ const app = express.Router();
 
 let ToDos = [];
 
-app.get("/", (req, res) => {
+app.get("/:ToDoId", (req, res) => {
+  const { ToDoId } = req.params;
+  if (ToDos[+ToDoId]) {
+    res.json(ToDos[+ToDoId]);
+  } else {
+    res.status(404).json("ToDo Item Not Found!");
+  }
   res.json(ToDos);
 });
 app.post("/", express.json(), (req, res) => {
@@ -12,6 +18,19 @@ app.post("/", express.json(), (req, res) => {
     res.json(ToDos.length - 1);
   } else {
     res.status(400).json("You have to enter text.");
+  }
+});
+app.put("/:ToDoId", express.json(), (req, res) => {
+  const { ToDoId } = req.params;
+  if (ToDos[+ToDoId]) {
+    if (typeof req.body.text !== "undefined") {
+      ToDos[+ToDoId] = req.body.text;
+      res.json(ToDos[+ToDoId]);
+    } else {
+      res.status(400).json("You have to enter text");
+    }
+  } else {
+    res.status(404).json("ToDo Item Not Found!");
   }
 });
 
